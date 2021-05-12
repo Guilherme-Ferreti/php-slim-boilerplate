ffa7b7c60610544003353044d294cfb58ac1c6a4
Guilherme Ferreti <guiferretis@gmail.com>
2021-05-12 13:44:38 -0300

    Comments and refactor to BaseValidator

--- src/Validations/BaseValidator.php
+++ src/Validations/BaseValidator.php
@@ -3,10 +3,12 @@
 namespace App\Validations;
 
 use Rakit\Validation\Validator; // For more information on how to use this class please see https://github.com/rakit/validation
+use Rakit\Validation\ErrorBag;
 
 abstract class BaseValidator
 {
     const RULES_DIRECTORY = __DIR__ . '/Rules';
+    const RULES_NAMESPACE = "\App\Validations\Rules\\";
 
     protected $validation;
 
@@ -29,6 +31,9 @@ abstract class BaseValidator
         $this->validation->validate();
     }
 
+    /**
+     * Adds custom validation rules to the validator object.
+     */
     protected function addCustomValidators(Validator $validator)
     {
         $files = scandir(self::RULES_DIRECTORY);
@@ -40,35 +45,56 @@ abstract class BaseValidator
 
             $file = str_replace('.php', '', $file);
 
-            $class = "\App\Validations\Rules\\$file";
+            $class = self::RULES_NAMESPACE . $file;
 
             $validator->addValidator(strtolower($file), new $class());
         }
     }
     
+    /**
+     * Returns if the validation has failed.
+     */
     public function fails() : bool
     {
         return $this->validation->fails();
     }
 
-    public function getErrors()
+    /** 
+     * Returns Rakit ErrorBag containing the validation errors.
+    */
+    public function getErrors() : ErrorBag
     {
         return $this->validation->errors();
     }
 
+    /**
+     * Retrivies data that was successfully validated.
+     */
     public function getValidData() : array
     {
         return $this->validation->getValidData();
     }
 
+    /**
+     * Treats data before validation.
+     */
     protected function sanitize(array $inputs) : array
     {
         return $inputs;
     }
 
+    /**
+     * Rules to be used for validation.
+     */
     abstract protected function rules() : array;
 
+    /** 
+     * Aliases to inputs being validated.
+    */
     abstract protected function aliases() : array;
 
+    /**
+     * Messages to be used for validation errors.
+     */
     abstract protected function messages() : array;
 }
