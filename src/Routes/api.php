987da90a6cb409ec420609dcb04f57ba603e9d76
Guilherme-Ferreti <guiferretis@gmail.com>
2021-06-06 10:24:50 -0300

    api.php with grouping examples

--- src/Routes/api.php
+++ src/Routes/api.php
@@ -4,7 +4,30 @@ use App\Controllers\HomeController;
 use App\Middlewares\CorsMiddleware;
 use Slim\Routing\RouteCollectorProxy;
 
-$app->group('/api', function (RouteCollectorProxy $group) {
+$app->group('/api', function (RouteCollectorProxy $route) {
+    $route->get('/home', new HomeController());
 
-    $group->get('/home', new HomeController());
+    $route->group('/fruits', function (RouteCollectorProxy $route) {
+        $route->get('[/]', function ($request, $response) {
+            $response->getBody()->write('Many fruits');
+            return $response;
+        });
+
+        $route->get('/apples', function ($request, $response) {
+            $response->getBody()->write('Apples');
+            return $response;
+        });
+
+        $route->get('/bananas', function ($request, $response) {
+            $response->getBody()->write('Bananas');
+            return $response;
+        });
+
+        $route->group('/others', function (RouteCollectorProxy $route) {
+            $route->get('/oranges', function ($request, $response) {
+                $response->getBody()->write('Oranges');
+                return $response;
+            });
+        });
+    });
 })->add(new CorsMiddleware());
